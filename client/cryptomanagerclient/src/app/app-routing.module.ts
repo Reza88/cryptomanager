f608b3b72db7803b33399f09ca2548f7214bdb69
ALi Farmani <Reza@ALis-MacBook-Pro.local>
2018-05-10 23:12:52 -0400

    Message / Registration / Data Service

--- client/cryptomanagerclient/src/app/app-routing.module.ts
+++ client/cryptomanagerclient/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path:'register',
+    component:RegistrationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/cryptomanagerclient/src/app/app.module.ts
+++ client/cryptomanagerclient/src/app/app.module.ts
@@ -4,17 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MessageComponent } from './message/message.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageComponent,
+    RegistrationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

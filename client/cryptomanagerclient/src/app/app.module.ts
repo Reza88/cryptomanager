1f6fca4d1538f66f913f43849be4422a1cd49c1d
mquercia <quercia.matt@gmail.com>
2018-05-10 23:09:18 -0400

    created navbar and added font awesome icons

--- client/cryptomanagerclient/src/app/app.module.ts
+++ client/cryptomanagerclient/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
